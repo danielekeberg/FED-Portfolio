28d0f779eef5ef6ea3130f8143db3411fd54a54c
Daniel Lehre Ekeberg <danieleke@live.no>
2024-12-10 14:31:08 +0100

    Update script.js
    Fixed minor hover issues where color doesnt match theme

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const toggleLight = document.getElementById("light-mode");
 const body = document.body;
+const header = document.querySelector("header");
 const projects = document.querySelectorAll(".my-project");
 const githubProfile = document.getElementById("githubProfile");
+const aboutme = document.querySelector(".aboutme");
+const allProjects = document.querySelector(".projects");
+const repoLinks = document.querySelectorAll(".github-repo a");
 
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
@@ -11,27 +15,49 @@ if (savedTheme) {
     projects.forEach((project) => {
       project.classList.add("light");
     });
+
+    repoLinks.forEach((link) => {
+        link.classList.add("aLight");
+    })
     toggleLight.src = "assets/sun-24-dark.png";
     githubProfile.src = "assets/github-10-24-dark.png";
+    header.style.borderBottom = "1px solid #0e0e0e";
+    allProjects.style.borderBottom = "1px solid #0e0e0e";
+    aboutme.style.borderBottom = "1px solid #0e0e0e";
   }
 }
 
 toggleLight.addEventListener("click", () => {
   if (body.classList.contains("light-mode")) {
     body.classList.remove("light-mode");
+    header.style.borderBottom = "1px solid #fff";
+    aboutme.style.borderBottom = "1px solid #fff";
+    allProjects.style.borderBottom = "1px solid #fff";
 
     projects.forEach((project) => {
       project.classList.remove("light");
     });
+
+    repoLinks.forEach((link) => {
+        link.classList.remove("aLight");
+    })
 
     localStorage.setItem("theme", "dark-mode");
     toggleLight.src = "assets/sun-24-light.png"; 
     githubProfile.src = "assets/github-10-24-light.png";
   } else {
     body.classList.add("light-mode");
+    header.style.borderBottom = "1px solid #0e0e0e";
+    aboutme.style.borderBottom = "1px solid #0e0e0e";
+    allProjects.style.borderBottom = "1px solid #0e0e0e";
+
     projects.forEach((project) => {
       project.classList.add("light");
     });
+
+    repoLinks.forEach((link) => {
+        link.classList.add("aLight");
+    })
 
     localStorage.setItem("theme", "light-mode");
     toggleLight.src = "assets/sun-24-dark.png";
