3f2e8606ef70820c5e73bb18ace9cb3a6ffdca14
Daniel Lehre Ekeberg <danieleke@live.no>
2024-12-10 15:08:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,52 +6,15 @@ const githubProfile = document.getElementById("githubProfile");
 const aboutme = document.querySelector(".aboutme");
 const allProjects = document.querySelector(".projects");
 const repoLinks = document.querySelectorAll(".github-repo a");
+const projectImages = document.querySelectorAll(".project-img");
 
 const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-    body.classList.add(savedTheme);
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+        aboutme.classList.add("aboutme-light");
 
-    if (savedTheme === "light-mode") {
-        projects.forEach((project) => {
-        project.classList.add("light");
-    });
-
-    repoLinks.forEach((link) => {
-        link.classList.add("aLight");
-    })
-    toggleLight.src = "assets/sun-24-dark.png";
-    githubProfile.src = "assets/github-10-24-dark.png";
-    header.style.borderBottom = "1px solid #0e0e0e";
-    allProjects.style.borderBottom = "1px solid #0e0e0e";
-    aboutme.style.borderBottom = "1px solid #0e0e0e";
-  }
-}
-
-toggleLight.addEventListener("click", () => {
-    if (body.classList.contains("light-mode")) {
-        body.classList.remove("light-mode");
-        header.style.borderBottom = "1px solid #fff";
-        aboutme.style.borderBottom = "1px solid #fff";
-        allProjects.style.borderBottom = "1px solid #fff";
-
-        projects.forEach((project) => {
-            project.classList.remove("light");
-    });
-
-    repoLinks.forEach((link) => {
-        link.classList.remove("aLight");
-    })
-
-    localStorage.setItem("theme", "dark-mode");
-    toggleLight.src = "assets/sun-24-light.png"; 
-    githubProfile.src = "assets/github-10-24-light.png";
-    } else {
-        body.classList.add("light-mode");
-        header.style.borderBottom = "1px solid #0e0e0e";
-        aboutme.style.borderBottom = "1px solid #0e0e0e";
-        allProjects.style.borderBottom = "1px solid #0e0e0e";
-
-        projects.forEach((project) => {
+        if (savedTheme === "light-mode") {
+            projects.forEach((project) => {
             project.classList.add("light");
         });
 
@@ -59,8 +22,50 @@ toggleLight.addEventListener("click", () => {
             link.classList.add("aLight");
         });
 
-        localStorage.setItem("theme", "light-mode");
         toggleLight.src = "assets/sun-24-dark.png";
         githubProfile.src = "assets/github-10-24-dark.png";
+        header.style.borderBottom = "1px solid #0e0e0e";
+        allProjects.style.borderBottom = "1px solid #0e0e0e";
+        aboutme.style.borderBottom = "1px solid #0e0e0e";
     }
+}
+
+toggleLight.addEventListener("click", () => {
+    if (body.classList.contains("light-mode")) {
+        body.classList.remove("light-mode");
+        aboutme.classList.remove("aboutme-light");
+        header.style.borderBottom = "1px solid #fff";
+        aboutme.style.borderBottom = "1px solid #fff";
+        allProjects.style.borderBottom = "1px solid #fff";
+
+        projects.forEach((project) => {
+            project.classList.remove("light");
+        });
+
+        repoLinks.forEach((link) => {
+            link.classList.remove("aLight");
+        });
+
+        localStorage.setItem("theme", "dark-mode");
+        toggleLight.src = "assets/sun-24-light.png"; 
+        githubProfile.src = "assets/github-10-24-light.png";
+        } else {
+            body.classList.add("light-mode");
+            aboutme.classList.add("aboutme-light");
+            header.style.borderBottom = "1px solid #0e0e0e";
+            aboutme.style.borderBottom = "1px solid #0e0e0e";
+            allProjects.style.borderBottom = "1px solid #0e0e0e";
+        
+            projects.forEach((project) => {
+                project.classList.add("light");
+            });
+        
+            repoLinks.forEach((link) => {
+                link.classList.add("aLight");
+            });
+        
+            localStorage.setItem("theme", "light-mode");
+            toggleLight.src = "assets/sun-24-dark.png";
+            githubProfile.src = "assets/github-10-24-dark.png";
+        }
 });
